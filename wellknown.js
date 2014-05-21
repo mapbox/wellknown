4aa291cbcc8e020f225b338b9490f08c6cb68b77
Tom MacWright <tom@macwright.org>
2014-05-21 17:40:15 -0400

    Add standalone

--- wellknown.js
+++ wellknown.js
@@ -1,5 +1,7 @@
-!function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var n;"undefined"!=typeof window?n=window:"undefined"!=typeof global?n=global:"undefined"!=typeof self&&(n=self),n.wellknown=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+!function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var n;"undefined"!=typeof window?n=window:"undefined"!=typeof global?n=global:"undefined"!=typeof self&&(n=self),n.wellknown=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports = parse;
+module.exports.parse = parse;
+module.exports.stringify = stringify;
 
  /*
  * Parse WKT and return GeoJSON.
@@ -27,7 +29,7 @@ function parse(_) {
         if (obj && srid.match(/\d+/)) {
             obj.crs = {
                 type: 'name',
-                'properties': {
+                properties: {
                     name: 'urn:ogc:def:crs:EPSG::' + srid
                 }
             };
@@ -42,6 +44,7 @@ function parse(_) {
         white();
         var depth = 0, rings = [], stack = [rings],
             pointer = rings, elem;
+
         while (elem =
             $(/^(\()/) ||
             $(/^(\))/) ||
@@ -50,21 +53,25 @@ function parse(_) {
             if (elem == '(') {
                 stack.push(pointer);
                 pointer = [];
-                stack[stack.length-1].push(pointer);
+                stack[stack.length - 1].push(pointer);
                 depth++;
             } else if (elem == ')') {
                 pointer = stack.pop();
+                // the stack was empty, input was malformed
+                if (!pointer) return;
                 depth--;
-                if (depth == 0) break;
+                if (depth === 0) break;
             } else if (elem === ',') {
                 pointer = [];
-                stack[stack.length-1].push(pointer);
-            } else {
+                stack[stack.length - 1].push(pointer);
+            } else if (!isNaN(parseFloat(elem))) {
                 pointer.push(parseFloat(elem));
+            } else {
+                return null;
             }
             white();
         }
-        stack.length = 0;
+
         if (depth !== 0) return null;
         return rings;
     }
@@ -92,6 +99,7 @@ function parse(_) {
         white();
         if (!$(/^(\()/)) return null;
         var c = coords();
+        if (!c) return null;
         white();
         if (!$(/^(\))/)) return null;
         return {
@@ -104,6 +112,7 @@ function parse(_) {
         if (!$(/^(multipoint)/i)) return null;
         white();
         var c = multicoords();
+        if (!c) return null;
         white();
         return {
             type: 'MultiPoint',
@@ -115,6 +124,7 @@ function parse(_) {
         if (!$(/^(multilinestring)/i)) return null;
         white();
         var c = multicoords();
+        if (!c) return null;
         white();
         return {
             type: 'MultiLineString',
@@ -127,6 +137,7 @@ function parse(_) {
         white();
         if (!$(/^(\()/)) return null;
         var c = coords();
+        if (!c) return null;
         if (!$(/^(\))/)) return null;
         return {
             type: 'LineString',
@@ -184,6 +195,56 @@ function parse(_) {
     }
 
     return crs(root());
+}
+
+/**
+ * Stringifies a GeoJSON object into WKT
+ */
+function stringify(gj) {
+    if (gj.type === 'Feature') {
+        gj = gj.geometry;
+    }
+
+    function pairWKT(c) {
+        if (c.length === 2) {
+            return c[0] + ' ' + c[1];
+        } else if (c.length === 3) {
+            return c[0] + ' ' + c[1] + ' ' + c[2];
+        }
+    }
+
+    function ringWKT(r) {
+        return r.map(pairWKT).join(', ');
+    }
+
+    function ringsWKT(r) {
+        return r.map(ringWKT).map(wrapParens).join(', ');
+    }
+
+    function multiRingsWKT(r) {
+        return r.map(ringsWKT).map(wrapParens).join(', ');
+    }
+
+    function wrapParens(s) { return '(' + s + ')'; }
+
+    switch (gj.type) {
+        case 'Point':
+            return 'POINT (' + pairWKT(gj.coordinates) + ')';
+        case 'LineString':
+            return 'LINESTRING (' + ringWKT(gj.coordinates) + ')';
+        case 'Polygon':
+            return 'POLYGON (' + ringsWKT(gj.coordinates) + ')';
+        case 'MultiPoint':
+            return 'MULTIPOINT (' + ringWKT(gj.coordinates) + ')';
+        case 'MultiPolygon':
+            return 'MULTIPOLYGON (' + multiRingsWKT(gj.coordinates) + ')';
+        case 'MultiLineString':
+            return 'MULTILINESTRING (' + ringsWKT(gj.coordinates) + ')';
+        case 'GeometryCollection':
+            return 'GEOMETRYCOLLECTION (' + gj.geometries.map(stringify).join(', ') + ')';
+        default:
+            throw new Error('stringify requires a valid GeoJSON Feature or geometry object as input');
+    }
 }
 
 },{}]},{},[1])
